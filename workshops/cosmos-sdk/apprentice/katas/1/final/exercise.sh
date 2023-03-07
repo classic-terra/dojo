0daf73633a1926ae140003cd435b6433d0d9ad28
ZaradarBH <tobias.andersen79@gmail.com>
2023-03-07 17:52:39 +0100

    - Finalize Protobuf  kata v1.0

--- workshops/cosmos-sdk/apprentice/katas/1/final/exercise.sh
+++ workshops/cosmos-sdk/apprentice/katas/1/final/exercise.sh
@@ -5,11 +5,17 @@ mkdir kata1
 #Change into the kata1 directory
 cd kata1
 
-#Install the protobuf library
-go get google.golang.org/protobuf/cmd/protoc-gen-go
+#Initialize a new go module
+go mod init example.com/proto
+
+#Install package dependencies
+go get google.golang.org/protobuf/proto
+go get google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 #Create a file called person.proto and add the below protobuf message contract
-PROTO_MSG="syntax = \"proto4\";
+PROTO_MSG="syntax = \"proto3\";
+option go_package = \"example.com/proto/main\";
 
 message Person {
   string name = 1;
@@ -18,3 +24,48 @@ message Person {
 }"
 
 echo "$PROTO_MSG" > person.proto
+
+#Generate the Go code from the protobuf contract
+protoc --go_out=. person.proto --go_opt=paths=source_relative
+
+#Implement a simple program
+GO_CODE="package main
+
+import (
+    \"fmt\"
+    \"log\"
+
+    \"google.golang.org/protobuf/proto\"
+)
+
+func main() {
+    p := &Person{
+        Name:    \"Alice\",
+        Age:     30,
+        Hobbies: []string{\"reading\", \"hiking\"},
+    }
+
+    data, err := proto.Marshal(p)
+    if err != nil {
+        log.Fatal(\"error marshaling person:\", err)
+    }
+
+    var p2 Person
+    err = proto.Unmarshal(data, &p2)
+    if err != nil {
+        log.Fatal(\"error unmarshaling person:\", err)
+    }
+
+    fmt.Printf(\"name: %s\n\", p2.Name)
+    fmt.Printf(\"age: %d\n\", p2.Age)
+    fmt.Printf(\"hobbies: %v\n\", p2.Hobbies)
+}"
+
+#Save the programn to a file called main.go
+echo "$GO_CODE" > main.go
+
+#Build the program
+go build
+
+#Run the program
+./proto
